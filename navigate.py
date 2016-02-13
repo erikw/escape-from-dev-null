66aa96f377be477b602b6d9ae10aecacb4ab5dd4
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-02-13 10:09:18 +0100

    Added scan function.

--- navigate.py
+++ navigate.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python2
 
 import sys
+import json
 from post import *
 
 ENTITY_ID = 'd4941687-ba87-4d67-9dfc-be00c67a0b36'
 
 last_cmd = None
+
+def scan_maze():
+    """Scan the current location.
+
+    .. Returns:
+    :returns: Whatever the API returns.
+
+    """
+    args = dict()
+    args["sessionid"] = SESSION_ID
+    args["entityid"] = ENTITY_ID
+    args["target"] = "scan"
+    resp = post(args)
+    for line in resp:
+        print line
+    return 0
+
 
 def command(cmd):
     """Send a command to the server.
@@ -69,6 +87,7 @@ def command(cmd):
 
 
     post(args)
+    scan_maze()
     return 0
 
 
