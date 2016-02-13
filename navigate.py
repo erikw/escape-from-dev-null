6a874b30a14990d8a9c5531cdc5a8c514014bb29
Erik Westrup <erik.westrup@gmail.com>
2016-02-13 12:07:17 +0100

    added navigate

--- navigate.py
+++ navigate.py
@@ -3,8 +3,31 @@ from post import *
 def move(direction):
     args = {}
     args['target']  = 'move'
-    args['direction']  = 'rup'
+    args['direction']  = direction
 
     resp = post(args)
 
 
+def up():
+    return move("up")
+
+def right_up():
+    return move("rup")
+
+def right():
+    return move("r")
+
+def right_down():
+    return move("rdown")
+
+def down():
+    return move("down")
+
+def left_down():
+    return move("ldown")
+
+def left():
+    return move("l")
+
+def left_up():
+    return move("lup")
