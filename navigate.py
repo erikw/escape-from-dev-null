5c66321ee656cfaaf898a5934c8a8bee0cc82ffa
Erik Westrup <erik.westrup@gmail.com>
2016-02-13 10:01:05 +0100

    Fixed bug

--- navigate.py
+++ navigate.py
@@ -5,6 +5,8 @@ from post import *
 
 ENTITY_ID = 'd4941687-ba87-4d67-9dfc-be00c67a0b36'
 
+last_cmd = None
+
 def navigate_maze(cmd):
     """Send a command to the server.
 
@@ -18,10 +20,11 @@ def navigate_maze(cmd):
     args = {}
     args['entityid'] = ENTITY_ID
     args['target']  = 'move'
+
     if cmd.startswith("w"):
         print("Moved up")
         args['direction']  = 'up'
-    if cmd.startswith("e"):
+    elif cmd.startswith("e"):
         print("Moved right up")
         args['direction']  = 'rup'
     elif cmd.startswith("a"):
@@ -42,6 +45,10 @@ def navigate_maze(cmd):
     elif cmd.startswith("c"):
         print("Moved right down")
         args['direction']  = 'rdown'
+    else:
+        print("wrong command")
+        return
+
 
     post(args)
     return 0
@@ -52,6 +59,10 @@ def main():
     try:
         while True:
             cmd = sys.stdin.readline()
+            cmd = cmd.strip()
+            if not cmd:
+                cmd = last_cmd
+            last_cmd = cmd
             navigate_maze(cmd)
 
     except (KeyboardInterrupt, SystemExit):
