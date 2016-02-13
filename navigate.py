e678dceb2c193f45cc7420d7d5ebb8cdaaa9115c
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-02-13 11:21:42 +0100

    Using global tokens in navigate.

--- navigate.py
+++ navigate.py
@@ -4,9 +4,8 @@ import sys
 import json
 from post import *
 from pprint import pprint
+from tokens import *
 
-SESSION_ID = '4ef7f78a-c305-471d-b139-6a1b9ddd4c7e'
-ENTITY_ID = 'd4941687-ba87-4d67-9dfc-be00c67a0b36'
 
 last_cmd = None
 
